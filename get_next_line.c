49a7f766b5e482344b5af5c1bf7ad8318ba38e8b
Gallien BOUDRIE <gboudrie@e1r3p7.42.fr>
2016-01-30 19:24:17 +0100

    jurfjufrfrbhif

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,42 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 19:52:53 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/01/19 20:33:10 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/01/30 19:24:00 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static void	*ft_addalloc(char **line, char *buf, int len)
+{
+	char	*new_line;
+
+	new_line = ft_strnew(ft_strlen(*line) + len - 1);
+	strcpy(new_line, *line);
+	strncat(new_line, buf, len);
+	free(*line);
+	line = &new_line;
+	return (line);
+}
+
 int			get_next_line(int const fd, char ** line)
 {
-	static char		*buf;
+	static char		*str;		
+	char			*buf[BUFF_SIZE];
 	int				rd;
 	int				i;
+	int				len;
 
-	if (buf[0] == '\0')
+	while ((rd = read(fd, buf, BUF_SIZE)) > 0)
 	{
-		rd = read(fd, buf, BUF_SIZE);
-		buf = (char *)malloc(rd);
-	}
-	i = 0;
-	while (buf[i] != \n)
-	{
-		if (buf[i] == '\0')
-		{
-			rd = read(fd, buf, BUF_SIZE);
-			buf = (char *)malloc(rd);
-		}
-		i++;
+		if (!(ft_strchr(buf, '\n')))
+			len = rd;
+		else
+			len = ft_strchr(buf, '\n') - buf;
+		if (str)
+			str = ft_addalloc(&str, buf, );
+		else
+			str = ft_strsub(buf, 0, rd);
+		if ()
 	}
 }
