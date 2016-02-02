b9d9b4b9711373e81883da92c3e840fd4eab6a11
Gallien BOUDRIE <gboudrie@e1r2p3.42.fr>
2016-02-02 20:28:10 +0100

    adding tests

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+int main(int ac, char **av)
+{
+	int fd;
+	char *str;
+	
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		ft_putendl("--- TEST AVEC FICHIER ---");
+		while (get_next_line(fd, str))
+			ft_putendl(str);
+		ft_putendl("-------------------------\n");
+		ft_putendl("--- TEST MAUVAIS FICH ---");
+		av[1] = NULL;
+		ft_putstr("gnl retourne : ");
+		ft_putnbr(get_next_line(fd, str));
+		ft_putendl("\n-------------------------\n");
+		close(fd);
+		ft_putendl("--- TEST AVEC CLAVIER ---");
+		fd = 0;
+		ft_putendl("Tapez la chaine a afficher : ");
+		get_next_line(fd, str);
+		ft_putstr("La chaine retournee est : ");
+		ft_putendl(str);
+		ft_putendl("-------------------------\n");
+	}
+	return (0);
+}
