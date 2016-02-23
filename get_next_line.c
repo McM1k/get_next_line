9243391b0ed71c52397351d88a358932c16a3b04
Gallien BOUDRIE <gboudrie@e2r7p6.42.fr>
2016-02-23 15:38:11 +0100

    frfrfr

--- get_next_line.c
+++ get_next_line.c
@@ -6,61 +6,56 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 19:52:53 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/02/22 18:54:10 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/02/23 15:33:43 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	allocLess(char **line, char *str)
+static int	allocLess(char **line, char **str)
 {
 	int		size;
 	char	*tmp;
-	
-	ft_putendl("poulet");
-	size = ft_strchr(str, '\n') - str;
-	ft_putendl("braisay");
-	*line = ft_strsub(str, 0, size);
-	ft_putendl("avec");
-	tmp = str;
-	str = ft_strsub(tmp, size + 1, ft_strlen(tmp) - (size + 1));
-	ft_putendl("sa soos");
+
+	size = ft_strchr(*str, '\n') - *str;
+	*line = ft_strsub(*str, 0, size);
+	tmp = *str;
+	*str = ft_strsub(tmp, size + 1, ft_strlen(tmp) - (size + 1));
 	ft_strdel(&tmp);
 	return (1);
 }
 
-static char	*allocMore(char *str, char *buf)
+static char	*allocMore(char **str, char *buf)
 {
 	char	*tmp;
 
-	tmp = str;
-	str = ft_strjoin(tmp, buf);
+	tmp = *str;
+	*str = ft_strjoin(tmp, buf);
 	ft_strdel(&tmp);
-	return (str);
+	return (*str);
 }
 
-static int	reader(int rd, char *str, int fd)
+static int	reader(int rd, char **str, int fd)
 {
 	char 	*buf;
 
-	ft_putendl("poulet");
 	if (!(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	if (!str)
+	if (!*str)
 	{
-		ft_putendl("braisay");
 		rd = read(fd, buf, BUFF_SIZE);
 		buf[rd] = '\0';
-		str = ft_strsub(buf, 0, BUFF_SIZE);
+		*str = ft_strsub(buf, 0, BUFF_SIZE);
 	}
-	while (!ft_strchr(str, '\n') && rd > 0)
+	while (!ft_strchr(*str, '\n') && rd > 0)
 	{
-		ft_putendl("avec");
 		rd = read(fd, buf, BUFF_SIZE);
-		buf[rd] = '\0';
-		str = allocMore(str, buf);
+		if (rd > 0)
+		{
+			buf[rd] = '\0';
+			*str = allocMore(str, buf);
+		}
 	}
-	ft_putendl("sa soos");
 	ft_strdel(&buf);
 	return (rd);
 }
@@ -69,20 +64,19 @@ int			get_next_line(int const fd, char ** line)
 {
 	static char		*str = NULL;
 	int				rd;
-	
-	if ((rd = reader(1, str, fd)) == -1)
+
+	if ((rd = reader(1, &str, fd)) == -1)
 		return (-1);
-	ft_putendl("oktamer");
-	ft_putendl(str);
-	if (ft_strchr(str, '\n'))
+	if (rd > 0)
 	{
-		ft_putendl("mdr1");
-		return (allocLess(line, str));
-	}
+		if (ft_strchr(str, '\n'))
+			return (allocLess(line, &str));
+		else
+		{
+			*line = ft_strdup(str);	
+			return (1);
+		}
+	}	
 	else
-	{
-		ft_putendl("mdr2");
-		*line = ft_strdup(str);
 		return (0);
-	}
 }
