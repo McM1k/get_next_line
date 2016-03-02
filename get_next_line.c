70c8ce3524c29ba63f79a4db353263cf329fef01
Gallien BOUDRIE <gboudrie@e1r11p15.42.fr>
2016-03-02 23:01:58 +0100

    hfrbgufgbhufg

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,41 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 19:52:53 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/02/23 16:16:26 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/03/02 23:01:45 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static t_fd	new_node(int fd)
+{
+	t_fd = new;
+
+	new = (t_fd *)ft_memalloc(sizeof(t_fd));
+	new->fd = fd;
+	new->str = ft_strnew(BUFF_SIZE);
+	new->next = NULL;
+	return (new);
+}
+
+static t_fd	is_node_existing(int fd, t_fd *begin)
+{
+	t_fd	*tmp;
+	t_fd	*node;
+
+	tmp = begin;
+	node = tmp;
+	while (node)
+	{
+		if (node->fd == fd)
+			return (node);
+		tmp = node;
+		node = node->next;
+	}
+	node = new_node(fd);
+	tmp->next = node;
+	return (node);
+}
 
 static int	alloc_less(char **line, char **str)
 {
@@ -25,63 +55,43 @@ static int	alloc_less(char **line, char **str)
 	return (1);
 }
 
-static char	*alloc_more(char **str, char *buf)
-{
-	char	*tmp;
-
-	tmp = *str;
-	*str = ft_strjoin(tmp, buf);
-	ft_strdel(&tmp);
-	return (*str);
-}
-
-static int	reader(int rd, char **str, int fd)
+static int	reader(int rd, t_fd node, int fd)
 {
 	char	*buf;
+	char	*tmp;
 
-	if (!(buf = ft_strnew(BUFF_SIZE)))
+	if (BUFF_SIZE <= 0 || !(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	if (!*str)
-	{
-		if ((rd = read(fd, buf, BUFF_SIZE)) == -1)
-			return (-1);
-		buf[rd] = '\0';
-		*str = ft_strsub(buf, 0, BUFF_SIZE);
-	}
-	while (!ft_strchr(*str, '\n') && rd > 0)
+	while (!ft_strchr(node->str, '\n') && rd > 0)
 	{
 		if ((rd = read(fd, buf, BUFF_SIZE)) == -1)
 			return (-1);
 		if (rd > 0)
 		{
 			buf[rd] = '\0';
-			*str = allocMore(str, buf);
+			tmp = node->str;
+			node->str = ft_strjoin(tmp, buf);
+			ft_strdel(&tmp);
 		}
 	}
 	ft_strdel(&buf);
-	if (ft_strlen(*str) > 0)
-		return (1);
-	return (0);
+	return (rd);
 }
 
 int			get_next_line(int const fd, char **line)
 {
-	static char		*str = NULL;
+	static t_fd		*start = NULL;
+	t_fd			*ptr;
 	int				rd;
 
-	if ((rd = reader(1, &str, fd)) == -1)
+	if (!start)
+		start = new_node(0);
+	rd = 1;
+	if ((rd = reader(rd, ptr, fd)) == -1)
 		return (-1);
-	if (rd > 0)
-	{
-		if (ft_strchr(str, '\n'))
-			return (allocLess(line, &str));
-		else
-		{
-			*line = ft_strdup(str);
-			*str = '\0';
-			return (1);
-		}
-	}
+	if (!ft_strchr(ptr->str, '\n'))
+		*line = ft_strsub(ptr->str, 0, ft_strlen(ptr->str));
 	else
-		return (0);
+		*line = ft_strsub(ptr->str, 0, ft_strchr(ptr->str, '\n') - ptr->str);
+	return (0);
 }
